fc661636ae751faebdcedf704979a534ea345c54
jhalak-surve <73473276+jhalak-surve@users.noreply.github.com>
2020-10-28 15:12:28 +0530

    Update polymorphism.php

--- polymorphism.php
+++ polymorphism.php
@@ -6,22 +6,22 @@
                 return $n1 + $n2;
             }
         }
-        else if( (is_string($n1)) && (is_string($n2)) ){
+        else if( (is_string($n1)) && ( (is_string($n2)) || (is_int($n2)) || (is_float($n2)) ) ){
             return $n1." ".$n2;
         }
+        else if( ((is_string($n1)) ||  (is_int($n1)) || (is_float($n1)) ) && ((is_string($n2)) )){
+            return $n1." ".$n2;
+        }
+        
         else{
             return "NOT POSSIBLE";
         }
         
     }
 
-    echo addition(2,5);
-    echo "<br>";
-    echo addition("jhalak","surve");
+    $n1 = "Jhalak";
+    $n2 = 5;
     
 
-    
-
-
-
+    echo addition($n1, $n2);
 ?>
