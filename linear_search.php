4c0dc350a4cc8212db3b7136c4543b7b6158fe76
jhalak-surve <73473276+jhalak-surve@users.noreply.github.com>
2020-10-28 14:16:24 +0530

    Update linear_search.php

--- linear_search.php
+++ linear_search.php
@@ -5,9 +5,9 @@
     
     //function for linear search
 
-    function linearSearch($ar,$ele){
+    function linearSearch($ar, $ele){
         sort($ar);
-        $length= count($ar);
+        $length = count($ar);
         if( ($ele < $ar[0]) || ($ele > $ar[$length-1]) ){
             return -1;
         }
@@ -22,7 +22,7 @@
       return -1;  
     } 
     
-    $result=linearSearch($ar , $ele);
+    $result = linearSearch($ar , $ele);
 
     if( $result == -1 ){
         echo "Element not found";
